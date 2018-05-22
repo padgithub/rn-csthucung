beba7cc4c0fd3745935b534b36dd115372890a9c
Thai Tuyen <tuyenathai@gmail.com>
2018-05-22 12:01:14 +0700

    àdsa

---
+++ src/Components/AppTabNavigator/ViewProfileTab/VEditProfile.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet
+} from "react-native";
+
+import { Icon } from 'native-base'
+
+
+class LikesTab extends Component {
+
+    static navigationOptions = {
+
+        tabBarIcon: ({ tintColor }) => (
+            <Icon name="ios-heart" style={{ color: tintColor }} />
+        )
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>LikesTab</Text>
+                <Text> con cac </Text>
+                
+            </View>
+        );
+    }
+}
+
+export default LikesTab;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
